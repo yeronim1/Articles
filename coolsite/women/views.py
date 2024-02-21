91174bb02c28de8218f04229d1bdc154fd8e8c3d
Yaroslav Morgun <morgunaroslav@gmail.com>
2024-02-21 10:54:37 +0200

    change views.py

--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
 
 
 from .forms import AddPostForm
@@ -44,7 +45,8 @@ def about(request):
 class AddPage(LoginRequiredMixin, DataMixin, CreateView):
     form_class = AddPostForm
     template_name = 'women/addpage.html'
-    login_url = '/admin/'
+    success_url = reverse_lazy('home')
+    login_url = reverse_lazy('home')
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
