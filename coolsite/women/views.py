718ebde0861e8acb3fdc776275f23c8a96a59af9
Yaroslav Morgun <morgunaroslav@gmail.com>
2024-02-27 20:35:54 +0200

    Addd django debug toolbar

--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -26,6 +26,9 @@ class WomenHome(DataMixin, ListView):
         c_def = self.get_user_context(title="Main page")
         return dict(list(context.items()) + list(c_def.items()))
 
+    def get_queryset(self):
+        return Women.objects.filter(is_published=True).select_related('cat')
+
 def about(request):
     contact_list = Women.objects.all()
     paginator = Paginator(contact_list, 3)
@@ -72,12 +75,13 @@ class WomenCategory(DataMixin, ListView):
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        c_def = self.get_user_context(title='Category - ' + str(context['posts'][0].cat),
-                                      cat_selected=context['posts'][0].cat_id)
+        c = Category.objects.get(slug=self.kwargs['cat_slug'])
+        c_def = self.get_user_context(title='Category - ' + str(c.name),
+                                      cat_selected=c.pk)
         return dict(list(context.items()) + list(c_def.items()))
 
     def get_queryset(self):
-        return Women.objects.filter(cat__slug=self.kwargs['cat_slug'], is_published=True)
+        return Women.objects.filter(cat__slug=self.kwargs['cat_slug'], is_published=True).select_related('cat')
 
 
 class RegisterUser(DataMixin, CreateView):
