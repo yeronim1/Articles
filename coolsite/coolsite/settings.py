abe78aaa113b1806e863c80c841a4d2c7d3fc88c
Yaroslav Morgun <morgunaroslav@gmail.com>
2024-03-01 15:39:52 +0200

    add cache

--- coolsite/coolsite/settings.py
+++ coolsite/coolsite/settings.py
@@ -134,3 +134,10 @@ LOGIN_REDIRECT_URL = '/'
 INTERNAL_IPS = [
     "127.0.0.1"
 ]
+
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.filebased.FileBasedCache",
+        "LOCATION": os.path.join(BASE_DIR, 'coolsite_cache'),
+    }
+}
--- coolsite/women/urls.py
+++ coolsite/women/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
 
 from .views import *
 
@@ -11,5 +12,5 @@ urlpatterns = [
     path('logout/', logout_user, name='logout'),
     path('register/', RegisterUser.as_view(), name='register'),
     path('post/<slug:post_slug>/', ShowPost.as_view(), name='post'),
-    path('category/<slug:cat_slug>/', WomenCategory.as_view(), name='category')
+    path('category/<slug:cat_slug>/', cache_page(60)(WomenCategory.as_view()), name='category')
 ]
